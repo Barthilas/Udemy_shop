3981955b9345f6e24f065378401c2cdbb3a41102
Barthilas-WORK <snetivy.martin@seznam.cz>
2021-11-02 15:14:02 +0100

    section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //attributes firts, then constructor, rest...
+
+  
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>("https://localhost:5001/api/products?pageSize=50").subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+        console.log(response);
+
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
