62f9baf4869eb79b6a733cbda28e314f4d3370db
Barthilas-WORK <snetivy.martin@seznam.cz>
2021-11-04 15:58:23 +0100

    wip sec9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -13,21 +10,12 @@ export class AppComponent implements OnInit {
 
   
   title = 'SkiNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
     
   }
 
   ngOnInit(): void {
-    this.http.get<IPagination>("https://localhost:5001/api/products?pageSize=50").subscribe(
-      (response: IPagination) => {
-        this.products = response.data;
-        console.log(response);
-
-    }, error => {
-      console.log(error);
-    })
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
+import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
+//services are decorated with the Injectable decorator
+//Singletons
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  baseUrl = "https://localhost:5001/api/";
+
+  constructor(private https: HttpClient) { }
+
+  getProducts(brandId?: number, typeId?: number)  {
+
+    return this.https.get<IPagination>(this.baseUrl + 'products');
+  }
+
+  getBrands() {
+    return this.https.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+  getTypes() {
+    return this.https.get<IType[]>(this.baseUrl + 'products/types');
+  }
 }
