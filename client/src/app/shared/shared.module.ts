54cb11fac7abb3ae39795f2932a684cd57981854
Barthilas-WORK <snetivy.martin@seznam.cz>
2021-11-08 10:59:49 +0100

    ep 100

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PaginationModule.forRoot() //forRoot - own providers array, needs to be injected into rootModule at startup, acting as a Singleton.
+  ],
+  exports: [
+    PaginationModule
   ]
 })
 export class SharedModule { }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { IBrand } from '../shared/models/brands';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
 
 //services are decorated with the Injectable decorator
 //Singletons
@@ -16,18 +17,19 @@ export class ShopService {
 
   constructor(private https: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number, sort?: string)  {
+  getProducts(shopParams: ShopParams)  {
     let params = new HttpParams();
-    if(brandId) {
-      params = params.append('brandId', brandId.toString());
+    if(shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
     }
-    if(typeId)
+    if(shopParams.typeId !== 0)
     {
-      params = params.append('typeId', typeId.toString());
+      params = params.append('typeId', shopParams.typeId.toString());
     }
-    if(sort) {
-      params = params.append('sort', sort);
-    }
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
+
 
     //pipe -> chain multiple rxjs operators.
     return this.https.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
