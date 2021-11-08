8b06b4cb233ab92980ff8a0bfb54d271b3ff136e
Barthilas-WORK <snetivy.martin@seznam.cz>
2021-11-08 16:01:57 +0100

    wip

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot() //forRoot - own providers array, needs to be injected into rootModule at startup, acting as a Singleton.
