05131ae461b8c6292e9b0aa7accdf80ae0df2fe9
Barthilas-WORK <snetivy.martin@seznam.cz>
2021-12-14 14:19:49 +0100

    section 17

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -5,14 +5,18 @@ using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using API.Dtos;
+using AutoMapper;
 
 namespace API.Controllers
 {
     public class BasketController : BaseController
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
             
         }
@@ -26,9 +30,10 @@ namespace API.Controllers
        
         [HttpPost]
         //IF ENDPOINT IS NOT BEING HIT CHECK CUSTOMERBASKET PROPERTIES, NASTY ERROR.
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket([FromBody] CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket([FromBody] CustomerBasketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customeBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customeBasket);
             return Ok(updatedBasket);
         }
        
